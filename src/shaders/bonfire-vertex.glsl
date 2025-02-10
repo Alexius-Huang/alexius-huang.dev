0d6724697d6a47d0e6ba2845e22dfac28743f77e
Alexius Huang <alexius.huang@gmail.com>
2025-02-10 23:24:32 +0100

    Add attenuation when spark is distant away from the bonfire center

--- src/shaders/bonfire-vertex.glsl
+++ src/shaders/bonfire-vertex.glsl
@@ -6,6 +6,8 @@ uniform float uSparkHeight;
 uniform float uSparkSizeMultiplier;
 uniform float uSparkSpeed;
 
+const float largestDistanceFromCenter = distance(vec2(.25, .25), vec2(.0));
+
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     modelPosition.y += uTime * (uSparkSpeed / 100.0);
@@ -16,6 +18,10 @@ void main() {
     gl_Position = projectedPosition;
 
     float scale = (1.0 - modelPosition.y / uSparkHeight) * uSparkSizeMultiplier;
-    gl_PointSize = uSize * uPixelRatio * scale;
+
+    float distanceToCenter = distance(modelPosition.xz, vec2(.0));
+    float attenuateFromDistance = 1.0 - (distanceToCenter / largestDistanceFromCenter);
+
+    gl_PointSize = uSize * uPixelRatio * scale * attenuateFromDistance;
     gl_PointSize /= -viewPosition.z;
 }
